f7138ded0f4599f4f7a5c35fdbb47cb074d2e596
tasukujp <tasuku.kakimoto@gmail.com>
2016-08-30 01:07:19 +0900

    deviseでログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable
+         :confirmable, :lockable, :timeoutable, authentication_keys: [:login]
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(['lower(name) = :value OR lower(email) = :value', { value: login.downcase }]).first
+    elsif conditions.key?(:name) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
   end
