6c482924531b56df04ae381c817e1bde2957701b
tasukujp <tasuku.kakimoto@gmail.com>
2016-10-10 15:28:59 +0900

    ルート定義に制約条件追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
 
-  resources :users, only: [:index, :show]
-  resources :books
+  resources :users, only: [:index, :show], constraints: { id: /\d+/ }
+  resources :books, constraints: { id: /\d+/ }
   resources :user_books, only: [:create, :update]
 
   root to: 'books#index'
