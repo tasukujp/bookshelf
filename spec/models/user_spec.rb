95d3f6b1e22befc45e895cc652e50aaada229847
tasukujp <tasuku.kakimoto@gmail.com>
2017-01-06 12:30:41 +0900

    create spec of user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many(:user_books) }
-  it { should have_many(:books).through(:user_books) }
-  it { should have_many(:reviews) }
-  it { should have_many(:review_books).through(:reviews) }
+  describe 'relationships' do
+    it { is_expected.to have_many(:user_books) }
+    it { is_expected.to have_many(:books).through(:user_books) }
+    it { is_expected.to have_many(:reviews) }
+    it { is_expected.to have_many(:review_books).through(:reviews) }
+  end
 
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
-  it { should validate_length_of(:name).is_at_least(4) }
-  it { should validate_length_of(:name).is_at_most(16) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:name).is_at_least(4).is_at_most(16) }
+    it { is_expected.to validate_length_of(:password).is_at_least(6).is_at_most(128) }
+    it { is_expected.to validate_confirmation_of(:password) }
+    it { is_expected.to allow_value('abc@example.com').for :email }
+    it { is_expected.not_to allow_value('abc').for :email }
+
+    context 'uniqueness' do
+      subject { build(:user) }
+      it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+      it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    end
+  end
 end
